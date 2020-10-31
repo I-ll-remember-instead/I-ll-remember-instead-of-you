2dd58b43332a9d29636f5aeae632277d069659c5
성범 <35191431+heosungbum@users.noreply.github.com>
2020-10-31 11:56:08 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,27 @@ using namespace std;
 using namespace anyalarm;
 
 int main(int argc, char* argv[]) {
-	int y, m, d, h, min,s;
-	scanf("%d %d %d %d %d %d", &y, &m, &d, &h, &min, &s);
+	TDList *a = new TDList();
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<int> ydis(2020, 2030);
+	std::uniform_int_distribution<int> mdis(1, 12);
+	std::uniform_int_distribution<int> ddis(28, 1);
+	std::uniform_int_distribution<int> hdis(0, 23);
+	std::uniform_int_distribution<int> mindis(0, 59);
+	std::uniform_int_distribution<int> sdis(0, 59);
+	
+	std::uniform_int_distribution<int> iudis(0, 99);
+	int y=ydis(gen), m=mdis(gen), d=ddis(gen), h=hdis(gen), min=mindis(gen), s=sdis(gen);
+	for(int i=0; i<100; i++){
+		y=ydis(gen); m=mdis(gen); d=ddis(gen); h=hdis(gen); min=mindis(gen); s=sdis(gen);
+		pushTDList(y,m,d,h,min,s,a,iudis(gen), iudis(gen), "message");
+	}
+	return 0;
+}
+
+void pushTDList(int y, int m, int d, int h, int min, int s, TDList * list, int i, int u, char * message){
+	//scanf("%d %d %d %d %d %d", &y, &m, &d, &h, &min, &s);
 	struct tm buffTm;
 	buffTm.tm_year=y-1900;
 	buffTm.tm_mon=m-1;
@@ -20,9 +39,5 @@ int main(int argc, char* argv[]) {
 		printf("요류");
 		return 0;
 	}
-	TDList *a = new TDList();
-	a->push(new Index(1,1,(char*)"d",term));
-	a->print();
-	return 0;
+	list->push(new Index(1,1,(char*)"d",term));
 }
-
